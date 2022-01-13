e15315798928ffc1931d3412d9ca80049ce68e2e
Emanuele Bertuol <emanuelebertuol@gmail.com>
2022-01-13 03:23:21 +0100

    General Commits

---
+++ src/Betfair/index.js
@@ -0,0 +1,111 @@
+const Betfair = require('betfair-api-node');
+
+const fetchBetfairOdds = async () => {
+  try {
+    const betfair = new Betfair(
+      'XAl3yHNF9KCFr2TI',
+      'ilmiosuccesso@gmail.com',
+      'pascal18',
+      true
+    );
+    await betfair.login();
+    // Getting full list of soccer events
+    let events = await betfair.listEvents({ eventTypeIds: ['1'] });
+
+    events = events.filter(
+      (event) =>
+        event.event.name === 'Salernitana - Lazio' ||
+        event.event.name === 'Central Coast Mariners - Newcastle Jets'
+    );
+
+    // For every event we get the avaiable markets (market catalogue)
+    for (let i = 0; i < events.length; i++) {
+      events[i].markets = await betfair.listMarketCatalogue(
+        {
+          eventIds: [events[i].event.id]
+        },
+        1000
+      );
+      // Filtering the market by keeping just those who are needed
+      events[i].markets = events[i].markets.filter(
+        (market) =>
+          market.marketName === 'Esito Finale' ||
+          market.marketName === 'Under/Over 2,5 gol' ||
+          market.marketName === 'Entrambe le Squadre Segnano'
+      );
+
+      // Getting all the back and lay data
+      for (let j = 0; j < events[i].markets.length; j++) {
+        // Esito finale
+        const objectValues = Object.values(events[i].markets[j]);
+
+        if (objectValues.length === 3 && objectValues[1] === 'Esito Finale') {
+          const oneXTwoOdds = await betfair.listMarketBook(
+            [events[i].markets[j].marketId],
+            {
+              priceProjection: {
+                priceData: ['EX_BEST_OFFERS']
+              }
+            }
+          );
+          events[i].oneXTwoOdds = oneXTwoOdds;
+        }
+        // Under/Over 2,5 goal
+        if (
+          objectValues.length === 3 &&
+          objectValues[1] === 'Under/Over 2,5 gol'
+        ) {
+          const underOverOdds = await betfair.listMarketBook(
+            [events[i].markets[j].marketId],
+            {
+              priceProjection: {
+                priceData: ['EX_BEST_OFFERS']
+              }
+            }
+          );
+          events[i].underOverOdds = underOverOdds;
+        }
+
+        // Goal/NoGoal odds
+        if (
+          objectValues.length === 3 &&
+          objectValues[1] === 'Entrambe le Squadre Segnano'
+        ) {
+          const goalNoGoalOdds = await betfair.listMarketBook(
+            [events[i].markets[j].marketId],
+            {
+              priceProjection: {
+                priceData: ['EX_BEST_OFFERS']
+              }
+            }
+          );
+          events[i].goalNoGoalOdds = goalNoGoalOdds;
+        }
+      }
+    }
+
+    const eventsData = [];
+    for (let event of events) {
+      const eventData = {
+        home: event.event.name.split(' - ')[0],
+        away: event.event.name.split(' - ')[1],
+        date: event.event.openDate,
+        oneOdds: event.oneXTwoOdds[0].runners[0].ex,
+        twoOdds: event.oneXTwoOdds[0].runners[1].ex,
+        xOdds: event.oneXTwoOdds[0].runners[2].ex,
+        underOdds: event.underOverOdds[0].runners[0].ex,
+        overOdds: event.underOverOdds[0].runners[1].ex,
+        goalOdds: event.goalNoGoalOdds[0].runners[0].ex,
+        noGoalOdds: event.goalNoGoalOdds[0].runners[1].ex
+      };
+
+      eventsData.push(eventData);
+    }
+
+    console.log(eventsData);
+  } catch (error) {
+    console.log(`An error occurred: ${error}`);
+  }
+};
+
+module.exports = fetchBetfairOdds;
--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const chrome = require('selenium-webdriver/chrome');
 const axios = require('axios');
 // Full Slot main scraper
 const fullSlotMainScraper = require('./Scraping Functions/fullSlotMainScraper');
+const fetchBetfairOdds = require('./Betfair/index.js');
 const sleep = require('./Utils/sleep.js');
 
 const main = async () => {
@@ -12,7 +13,7 @@ const main = async () => {
     const start = window.performance.now();
     console.log('Starting the main scraper function.');
     try {
-      await fullSlotMainScraper(
+      const fullSlotOdds = await fullSlotMainScraper(
         chrome,
         Builder,
         By,
@@ -25,6 +26,11 @@ const main = async () => {
       await axios.post(
         'https://mybet21-fullslotnew-be.herokuapp.com/odds/post-full-new-slot-odds',
         fullSlotOdds
+      );
+      const betfairOdds = await fetchBetfairOdds();
+      await axios.post(
+        'https://mybet21-fullslotnew-be.herokuapp.com/odds',
+        betfairOdds
       );
       const stop = window.performance.now();
       console.log(`Time Taken to execute = ${(stop - start) / 1000} seconds`);
