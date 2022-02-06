23bca9ff588aa3792bae9691db5e98029a876664
Emanuele Bertuol <emanuelebertuol@gmail.com>
2022-02-06 19:15:20 +0100

    Implementing the code to open all the desidered tournaments

--- src/Scraping Functions/openSoccerSideBar.js
+++ src/Scraping Functions/openSoccerSideBar.js
@@ -1,9 +1,15 @@
 const { By, until } = require('selenium-webdriver');
+// Scraping functions
+const openTournaments = require('./openTournaments');
 
 const openSoccerSideBar = async (driver) => {
   const sidebar = await driver.wait(
     until.elementLocated(By.id('palinsesto-prematch-container'))
   );
+  const soccerSection = await sidebar.findElement(By.id('category-1'));
+  await soccerSection.click();
+
+  await openTournaments(driver, sidebar);
 };
 
 module.exports = openSoccerSideBar;
---
+++ src/Scraping Functions/openTournaments.js
@@ -0,0 +1,48 @@
+const { By, until } = require('selenium-webdriver');
+const nationNames = require('../Utils/nationNames');
+
+const openTournaments = async (driver, sidebar) => {
+  const nations = await sidebar.findElements(
+    By.className('accordion-content mg-palinsesto-nazione')
+  );
+
+  for (let nation of nations) {
+    const nationName = await nation.findElements(By.css('span'));
+
+    if (nationNames[await nationName[0].getAttribute('innerText')]) {
+      let checkIfIsLoading = true;
+      while (checkIfIsLoading) {
+        try {
+          await driver.findElement(
+            By.className('mg-caricamento-eventi-maschera mg-maschera-prematch')
+          );
+        } catch (error) {
+          checkIfIsLoading = false;
+          if (
+            (await nation.getAttribute('class')) !==
+            'accordion-content mg-palinsesto-nazione mg-palinsesto-nazione-open'
+          ) {
+            await driver.executeScript(
+              'arguments[0].scrollIntoView(true);',
+              nation
+            );
+            await nation.click();
+
+            const openAllTournamentsButton = await nation.findElement(
+              By.className('openall')
+            );
+
+            await driver.executeScript(
+              'arguments[0].scrollIntoView(true);',
+              openAllTournamentsButton
+            );
+            await openAllTournamentsButton.click();
+          } else {
+          }
+        }
+      }
+    }
+  }
+};
+
+module.exports = openTournaments;
